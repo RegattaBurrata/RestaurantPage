349a7869f72062e8048c8af4ba54c6a13078982c
Dan Vidmar <65089365+RegattaBurrata@users.noreply.github.com>
2023-01-27 15:44:20 -0800

    Create pageload module

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("hello");
+(()=>{"use strict";!function(){const e=document.querySelector("#content"),t=(e,t)=>{const n=document.createElement(e);return n.classList.add(t),n},n=t("div","container"),d=t("div","header-container"),i=t("div","body-container"),c=t("div","body-title");t("p","title"),t("div","body-content"),t("p","description"),e.appendChild(n),n.appendChild(d),n.appendChild(i),i.appendChild(c)}()()})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello');
+import pageLoad from './pageload';
+
+pageLoad();
